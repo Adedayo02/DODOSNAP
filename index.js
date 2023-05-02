0ea0ffd04e10e95abfe245b4532fbb1f7686fa0a
unknown <landesanmi02@gmail.com>
2023-05-02 16:19:48 +0100

    Radio message

---
+++ index.js
@@ -0,0 +1,10 @@
+    var nav = document.getElementsByClassName("nav-link_wrapper");
+    var button = document.getElementById("ham");
+
+    button.onclick = function(){
+
+
+    };
+
+    console.log(nav);
+    console.log(button);
